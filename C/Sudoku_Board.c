d5ddc550054e54429ea55969a662cb0aba2e2fa0
Hayden <haycfry@gmail.com>
2025-08-11 11:51:03 -0500

    modified:   C/Sudoku_Board.c 	modified:   C/list of features to be implamented.txt 	modified:   C/output/Sudoku_Board.exe 	new file:   Sudoku/Create_Board.spec 	new file:   Sudoku/build/Create_Board/Analysis-00.toc 	new file:   Sudoku/build/Create_Board/Create_Board.pkg 	new file:   Sudoku/build/Create_Board/EXE-00.toc 	new file:   Sudoku/build/Create_Board/PKG-00.toc 	new file:   Sudoku/build/Create_Board/PYZ-00.pyz 	new file:   Sudoku/build/Create_Board/PYZ-00.toc 	new file:   Sudoku/build/Create_Board/base_library.zip 	new file:   Sudoku/build/Create_Board/localpycs/pyimod01_archive.pyc 	new file:   Sudoku/build/Create_Board/localpycs/pyimod02_importers.pyc 	new file:   Sudoku/build/Create_Board/localpycs/pyimod03_ctypes.pyc 	new file:   Sudoku/build/Create_Board/localpycs/pyimod04_pywin32.pyc 	new file:   Sudoku/build/Create_Board/localpycs/struct.pyc 	new file:   Sudoku/build/Create_Board/warn-Create_Board.txt 	new file:   Sudoku/build/Create_Board/xref-Create_Board.html 	new file:   Sudoku/dist/Create_Board.exe

--- C/Sudoku_Board.c
+++ C/Sudoku_Board.c
@@ -5,8 +5,10 @@
 
 void shuffle(int *array, size_t n);
 int check_columns(int (*board)[9]);
+int check_squares(int (*board)[9]);
 
 int main() {
+    clock_t start_time = clock();
     srand(time(NULL)); // seed the random number generator
     int board[9][9]; // 9x9 board
     for (int column = 0; column < 9; column++) {
@@ -15,9 +17,12 @@ int main() {
         }
     }  
 
+
+
     for (int column = 0; column < 9; column++) {// fill the board with random numbers between 1 and 9
-        int column_valid = 0;
-        while (column_valid == 0)
+        int column_valid = 0, squares_valid = 0, attempts = 0;
+
+        while (column_valid == 0 || squares_valid == 0)
         {
 
             int numbers[] = {1, 2, 3, 4, 5, 6, 7, 8, 9}; // create an array of numbers to shuffle
@@ -26,9 +31,22 @@ int main() {
             for (int row = 0; row < 9; row++) {
                 board[column][row] = numbers[row]; // put the shuffled numbers into the board
             }
+        
 
             column_valid = check_columns(board); // check if columns valid like this
+            squares_valid = check_squares(board); // check if 3x3 grids valid
+            attempts++;
+            if (attempts > 100000){
+                column_valid = 1, squares_valid = 1;
+                for (int column = 0; column < 9; column++) {
+                    for (int row = 0; row < 9; row++) {
+                        board[column][row] = 0; // initialize the board with zeros
+                    }
+                }
+                column = -1;  
+            }
         }
+        
     }
     
 
@@ -42,6 +60,11 @@ int main() {
             printf("\n");
         } 
     }
+    clock_t end_time = clock();
+    double time_used = (double)(end_time - start_time) / CLOCKS_PER_SEC;
+    printf("\nTime used to create board %f seconds", time_used);
+    printf("\npress enter to exit: ");
+    getchar();
     return 0;
 }
 
@@ -84,4 +107,43 @@ int check_columns(int (*board)[9]){
      
     }
     return 1;   
+}
+
+int check_squares(int (*board)[9]){
+    int square_x = 0, square_y = 0;
+    while (1)
+    {
+    int numbers[] = {0,0,0,0,0,0,0,0,0};
+    for (int column = 0; column < 3; column++)
+    {
+        for (int row = 0; row < 3; row++)
+        {
+            int number = board[column + square_x][row + square_y]; // find number in cell
+            if (number != 0){
+                numbers[number - 1] += 1; // add 1 count to number of times it's used
+            }
+        }
+        
+        }
+    
+    for (int i = 0; i < 9; i++){
+    if (numbers[i] > 1){
+        return 0; // return false if a number appeared more than once in a 3x3 grid
+        }
+    }
+
+    if (square_y == 6){
+        return 1; // every 3x3 checked so return true
+    }
+
+    if (square_x == 6){
+        square_y += 3;
+        square_x = 0;
+    }
+
+    else{
+        square_x += 3;
+    }
+
+    }// end of while true    
 }
